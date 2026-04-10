ff5848e5e967ba6aef1865947ed489678131c0a3
Wawans <wakuadratn@gmail.com>
2026-04-10 15:58:27 +0700

    wip: master referensi

---
+++ lib/utils.ts
@@ -0,0 +1,14 @@
+import { isString, trimString, trimStringWithEllipsis } from './stringUtils';
+import { isRegExp, isObject, isError, } from './rtti';
+import { urlMatches, urlMatchesEqual } from './urlMatch';
+
+export function trimUrl(param: any): string | undefined {
+    if (isRegExp(param))
+        return `/${trimStringWithEllipsis(param.source, 50)}/${param.flags}`;
+    if (isString(param))
+        return `"${trimStringWithEllipsis(param, 50)}"`;
+}
+
+export function matchUrl(urlOrPredicate: string | RegExp, responseUrl: string, baseURL: string): boolean {
+    return urlMatches(baseURL, responseUrl, urlOrPredicate);
+}
